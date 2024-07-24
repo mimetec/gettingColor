df6e71bd925cf3ae3f1820fc90b6d63adeabd61c
Danielle Lombardo <danielle@daniellelombardo.com>
2024-07-24 15:09:16 -0700

    adding some basics

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Danielle');
+  const [person, setPerson] = useState({ name: 'Mario', age: 43 })
+
+  const clickHandler = () => {
+    setName('Marc');
+    setPerson({ name: 'Luigi', age: 37 })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>gettingColor</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Welcome {name}!</Text>
+        <Text>His name is {person.name} and his age is {person.age} This is our body, what happens if there's more text than width of the screen</Text>
+        <Text>This is our body</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='update state name' onPress={clickHandler}/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +36,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'green',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'lightgreen',
+    color: 'white',
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# gettingColor
+
+`cd C:\Users\Danielle\OneDrive\Desktop\repos\gettingColor`
+
+`npm start` to start expo CLI or `npx expo start`
